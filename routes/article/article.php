c08e5337faae32ec637e002eb9c85e382d0e02ae
thongnguyen204 <spxdialga@gmail.com>
2025-03-29 02:57:55 +0700

    update: route polices

--- routes/article/article.php
+++ routes/article/article.php
@@ -4,20 +4,21 @@ use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'as' => 'articles.',
-    'prefix' => 'articles',
+    'as'         => 'articles.',
+    'prefix'     => 'articles',
     'controller' => ArticleController::class,
 ], function () {
     Route::get('/', 'index')->name('index');
     Route::middleware(['auth'])->group(function () {
         Route::get('/create', 'create')->name('create');
-        Route::post('/','store')->name('store');
+        Route::post('/', 'store')->name('store');
         Route::get('/{article}', 'show')->name('show')
             ->can('view', 'article');
-        Route::get('/{article}/edit', 'edit')->name('edit');
-        Route::put('/{article}', 'update')->name('update');
-        Route::delete('/{article}', 'destroy')->name('destroy');
+        Route::get('/{article}/edit', 'edit')->name('edit')
+            ->can('update', 'article');
+        Route::put('/{article}', 'update')->name('update')
+            ->can('update', 'article');
+        Route::delete('/{article}', 'destroy')->name('destroy')
+            ->can('delete', 'article');
     });
 });
-
-
--- routes/article/search.php
+++ routes/article/search.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'controller' => ArticleController::class,
-    'middleware' => 'auth'
+    'middleware' => 'auth',
 ], function () {
     Route::get('/search/{user}', 'indexByUser')->name('articlesByUser.index');
 });
